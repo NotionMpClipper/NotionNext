5549295b138acceecdcc44ce5b2262c2eafc133e
NotionMpClipper <166299585+NotionMpClipper@users.noreply.github.com>
2024-04-08 20:33:12 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -15,7 +15,7 @@ const CONFIG = {
   STARTER_HERO_TITLE_2: '极力推荐的Notion移动端剪藏插件', // 英雄区文字
   // 英雄区两个按钮，如果TEXT留空则隐藏按钮
   STARTER_HERO_BUTTON_1_TEXT: '开始体验', // 英雄区按钮
-  STARTER_HERO_BUTTON_1_URL: 'https://docs.tangly1024.com/article/vercel-deploy-notion-next', // 英雄区按钮
+  STARTER_HERO_BUTTON_1_URL: 'https://630go.top/article/start', // 英雄区按钮
   STARTER_HERO_BUTTON_2_TEXT: '', // 英雄区按钮
   STARTER_HERO_BUTTON_2_URL: '', // 英雄区按钮
   STARTER_HERO_BUTTON_2_ICON: '', // 英雄区按钮2的图标，不需要则留空
